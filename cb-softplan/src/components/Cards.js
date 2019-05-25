1a5ab0d1300ffb2c0838461f25e94dff03195bfb
Camila Barreto <camila.camilabarreto@gmail.com>
2019-05-25 16:43:31 -0300

    [Dev] - Cards Components - organize

--- cb-softplan/src/components/Cards.js
+++ cb-softplan/src/components/Cards.js
@@ -1,38 +1,22 @@
 import React from 'react';
-import PlusIcon from './icons/PlusIcon';
-import FolderIcon from './icons/FolderIcon';
-import TagIcon from './icons/TagIcon';
+import CardsInfo from './CardsInfo';
+import CardsFolder from './CardsFolder';
+import CardsTag from './CardsTag';
 
-const Cards = (props) => {
-    return(
-        <div className="cards">
-            <div className="cards__wrapper">
-                <div className="cards__info">
-                    <div className="info__name">
-                        <PlusIcon />
-                        <p>Felipe Genuino</p>
-                    </div>
-                    <div className="info__procedure">
-                        <p>Procedimento Comum - <span>Requerimento de Reintegração de Posse</span></p>
-                    </div>
-                    <div className="info__extra">
-                        <p className="extra__label">27/12/2018</p>
-                        <p className="extra__badge">User Interface</p>
-                        <p className="extra__badge extra__badge--red">Estatuto Idoso</p>
-                    </div>
-                </div>
-                <span className="cards__spacer"></span>
-                <div className="cards__folder">
-                    <FolderIcon />
-                    <p>Abrir pasta</p>
-                </div>
-                <span className="cards__spacer"></span>
-                <div className="cards__tag">
-                    <TagIcon />
+class Cards extends React.Component {
+    render() {
+        return(
+            <div className="cards">
+                <div className="cards__wrapper">
+                    <CardsInfo />
+                    <span className="cards__spacer"></span>
+                    <CardsFolder />
+                    <span className="cards__spacer"></span>
+                    <CardsTag />
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Cards;
---
+++ cb-softplan/src/components/CardsTag.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TagIcon from './icons/TagIcon';
+
+const CardsTag = (props) => {
+    return(
+        <div className="cards__tag">
+            <TagIcon />
+        </div>
+    )
+}
+
+export default CardsTag;
