cce6f02ef65f2554aec4878b46b4748fff5df4a9
Camila Barreto <camila.camilabarreto@gmail.com>
2019-05-25 21:01:07 -0300

    [Dev] - Mobile Styles

--- cb-softplan/src/components/SearchInput.js
+++ cb-softplan/src/components/SearchInput.js
@@ -3,8 +3,7 @@ import React from 'react';
 const SearchInput = () => {
     return(
         <div className="search__group">
-            <label for="busca">Buscar</label>
-            <input type="search" />
+            <input type="text" placeholder="Buscar" />
         </div>
     )
 }
