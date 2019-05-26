0dca0c90b4e0a71ed42a3dc13be0ec7a8f7513f9
Camila Barreto <camila.camilabarreto@gmail.com>
2019-05-26 08:01:43 -0300

    [Dev] - App.js file structure

--- cb-softplan/src/App.js
+++ cb-softplan/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Header from './components/Header';
+import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello!</p>
+      <Header />
+      <div className="middleWrapper">
+        <Sidebar />
+        <section className="contentSection">
+          <Topbar />
+          <Container>
+            <Cards />
+          </Container>
+        </section>
+      </div>
     </div>
   );
 }
